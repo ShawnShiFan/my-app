3f2557e7649ae8fcf6aea159230fc3e43d946ec1
Shawn <shawn_shifan@163.com>
2019-03-15 12:54:18 +0800

    实例react结合es6 代码优化升级

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,7 @@
 import React ,{Fragment} from 'react';
+import TodoItem from './TodoItem';
+import './style.css'
+
 
 class TodoList extends React.Component{
 
@@ -8,63 +11,81 @@ class TodoList extends React.Component{
 			inputValue:'',
 			list:[]
 		}
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleBtnClick = this.handleBtnClick.bind(this);
+		this.handleItemDelete = this.handleItemDelete.bind(this)
 
 	}
-
-		render(){
-			return(
-				<Fragment>
-						<div>
-						<input 
+	render(){
+		return(
+			<Fragment>
+				{/* 这是一个注释*/}
+				<div>
+					<label htmlFor="insertArea">输入内容：</label>
+					<input
+						id="insertArea"
+						className='input'
 						value={this.state.inputValue}
-						onChange={this.handleInputChange.bind(this)}
-						 />
-						
-						<button onClick={this.handleBtnClick.bind(this)}>提交</button>
-						</div>
-				 <ul>
-				 	{this.state.list.map((item, index) =>{
-				 		return <li 
-						 		key={index} 
-						 		onClick={this.handleItemDelete.bind(this, index)}
-						 		>
-						 		{item}
-						 		</li>
-				 	})}
-				 </ul>
-				</Fragment>
-				);
-		}
+						onChange={this.handleInputChange}
+					/>
 
+					<button onClick={this.handleBtnClick}>提交</button>
+				</div>
+				<ul>
+					{this.getTodoItem()}
+				</ul>
+			</Fragment>
+		);
+	}
 
-		handleInputChange(e){
-			this.setState({
-				inputValue: e.target.value
+	getTodoItem(){
+		return(
+			this.state.list.map((item, index) =>{
+				return(
+					<TodoItem
+						key={index}
+						content={item}
+						index={index}
+						deleteItem={this.handleItemDelete}
+					/>
+				)
 			})
-		}
-		
-		handleBtnClick(){
-			this.setState({
-				list:[...this.state.list,this.state.inputValue],
-				inputValue:''
-			})
-		}
-
-		handleItemDelete(index){
-				//immutable
-				//state 不允许我们做任何的改变
-				//如果实在要改变state中的值，那么可以复制一个副本出来修改副本
-				const list = [...this.state.list]; //把list拷贝到
-				list.splice(index,1);   //splice 删除index对应的1项
-				//this.state.list.splice(index,1)  这种方式React是不允许这样操作的，性能优化问题
-				this.setState({
-					list: list
-				})
-
-		}
+		);
 
 
 	}
+	handleInputChange(e){
+		/*	this.setState({
+                inputValue: e.target.value
+            })*/
+		//新版语法 setstate里面可以接收一个函数 但是是异步操作，所以数据放外面先传进来
+		const value = e.target.value;
+		this.setState(() =>({
+			//此处在es6中精简写法返回用括号代替return
+			inputValue:value
+		}))
+	}
+
+	handleBtnClick(){
+		this.setState((prevState) =>({
+			list:[...prevState.list,prevState.inputValue],
+			inputValue:''
+		}))
+	}
+
+	handleItemDelete(index){
+		//immutable
+		//state 不允许我们做任何的改变
+		//如果实在要改变state中的值，那么可以复制一个副本出来修改副本
+		//this.state.list.splice(index,1)  这种方式React是不允许这样操作的，性能优化问题
+		this.setState((prevState) =>{
+			const  list = [...prevState.list] //把list拷贝到
+			list.splice(index,1);   //splice 删除index对应的1项
+			return {list}
+		});
+	}
+
+}
 
 
 
